cadb6b8ab25555205e4ff50860ae642d439318b8
Arup Deb <arupdeb20010@gmail.com>
2022-06-26 13:04:06 +0530

    Add files via upload

---
+++ NameList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+function NameList (){ 
+  const names = ['Bruce', 'clark','Djana']
+  const nameList = names.map(name => <h2>{name}</h2>)
+    return (
+      <div>
+        {/* <h2>{names[0]}</h2>
+        <h2>{names[1]}</h2>
+        <h2>{names[2 ]}</h2> */}
+       nameList
+      </div>
+    )
+  }
+
+
+export default NameList
